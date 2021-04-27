5b3aab329667f827ea86326db055ba8387ab3fc1
nestor <nestor_agut@hotmail.com>
2021-04-27 09:59:44 +0200

    better explanation commit

--- syncfoldersonedir.sh
+++ syncfoldersonedir.sh
@@ -12,7 +12,10 @@ else
 	echo "Are you sure that you want to sync it? (y/n)"
 	read Answer
 	if [ Answer = "y" ]; then
+        echo "the folders have been sincroniced "
 		rsync -avu $Dir1 $Dir2
+    else
+        echo "the folders have not been sincroniced"
 	fi
 fi
 	
